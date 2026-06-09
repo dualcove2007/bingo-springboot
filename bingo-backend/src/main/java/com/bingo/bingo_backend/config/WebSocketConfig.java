759a59e8870a3469ffc2909b438dd19775232c46
dualcove2007 <dualcove@gmail.com>
2026-06-09 18:41:28 -0500

    fix: websocket handler con wildcard para rutas dinamicas

--- bingo-backend/src/main/java/com/bingo/bingo_backend/config/WebSocketConfig.java
+++ bingo-backend/src/main/java/com/bingo/bingo_backend/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.bingo.bingo_backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
@@ -15,7 +17,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(bingoHandler, "/ws/lobby/{playerName}", "/ws/juego/{playerName}")
+        registry.addHandler(bingoHandler, "/ws/**")
                 .setAllowedOrigins("*");
     }
 }
