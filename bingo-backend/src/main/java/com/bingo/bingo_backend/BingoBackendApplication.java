b5cf278224ee6a909f4b786428ceb710e85e7e92
dualcove2007 <dualcove@gmail.com>
2026-06-09 17:25:41 -0500

    init: proyecto spring boot base

---
+++ bingo-backend/src/main/java/com/bingo/bingo_backend/BingoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bingo.bingo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoBackendApplication.class, args);
+	}
+
+}
